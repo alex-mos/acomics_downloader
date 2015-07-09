c542c119e02c101bde89bcee74bab1c02852fbc2
alexander.mospan <alexander.mospan@gmail.com>
2015-07-09 12:57:09 +0300

    suitable for any comics

--- downloader.py
+++ downloader.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Скрипт для скачивания комиксов про гоблинов.
+Скрипт для скачивания комиксов c acomics.ru.
 """
 
 import requests
 import re
 
-comix_url = 'http://acomics.ru/~goblins/'
-first_page_number = 659
-last_page_number = 842
+comix_title = 'triangle'
+comix_url = 'http://acomics.ru/~' + comix_title + '/'
+first_page_number = 1
+last_page_number = 327
 
 
 def page_download(comix_link, page_num):
     page_url = comix_link + str(page_num)
     response = requests.get(page_url, cookies={'ageRestrict': '17'})
     page_content = response.content.decode('utf-8')
-    link_regex = '/upload/!c/!import/goblins/000' + str(page_num) + '-.+.jpg'
+    link_regex = '/upload/!c/.+/' + comix_title + '/' + ('%6d' % page_num).replace(' ', '0') + '-.+.jpg'
     img_link = re.search(link_regex, page_content)
-    try:
-        img_link_abs = 'http://acomics.ru' + img_link.group()
-    except:
-        link_regex = '/upload/!c/Duke/goblins/000' + str(page_num) + '-.+.jpg'
-        img_link = re.search(link_regex, page_content)
-        img_link_abs = 'http://acomics.ru' + img_link.group()
+    img_link_abs = 'http://acomics.ru' + img_link.group()
 
     img_code = requests.get(img_link_abs)
 
